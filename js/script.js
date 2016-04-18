0fafabd021034f3e2069a6bf93ef0a0eacc72bc6
williamyeny <williamyeny@gmail.com>
2016-04-17 23:33:04 -0400

    added copy to clipboard

--- js/script.js
+++ js/script.js
@@ -1,19 +1,28 @@
 var col;
 var hex;
+var clip;
 
 $(document).ready(function() {
+  
+  //setup copy
+  clip = new Clipboard('#text');
+  
   changeColor();
   
   $('#gen').click(function() {
     changeColor();
   });
+  
+  clip.on('success', function(e) {
+    $('#text').html(hex + ' - copied');
+  });
 });
 
 function changeColor() {
-  col = parseInt(Math.random() * 360);
+  col = parseInt(Math.random() * 360); //randomize color
 
-  $('body').css('background-color', 'hsla(' + col + ', 100%, 90%, 1)');
+  $('body').css('background-color', 'hsla(' + col + ', 100%, 90%, 1)'); //set color
   
-  hex = '#' + tinycolor('hsl(' + col + ', 100%, 90%)').toHex();
-  $('#text').html(hex);
+  hex = '#' + tinycolor('hsl(' + col + ', 100%, 90%)').toHex(); //translate to hex
+  $('#text').html(hex); //set text
 }
