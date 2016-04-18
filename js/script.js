2994340dd4d925dabd265436f5136530ed26be7c
williamyeny <williamyeny@gmail.com>
2016-04-18 11:33:53 -0400

    auto generate

--- js/script.js
+++ js/script.js
@@ -1,16 +1,45 @@
 var col;
 var hex;
 var clip;
+var auto;
+var speed;
 
 $(document).ready(function() {
   
   //setup copy
   clip = new Clipboard('#text');
   
+  //setup auto 
+  auto = false;
+  speed = 1;
+  
   changeColor();
   
-  $('#gen').click(function() {
-    changeColor();
+  $('#gen').click(function(e) {
+    e.preventDefault();
+    if (!auto) {
+      changeColor(); //if it's on manual, just change
+    } else {
+      if (speed == 5) { //max speed value
+        speed = 0;
+      }
+      speed++;
+      $('#gen').html('speed: ' + speed);
+    }
+  });
+
+  $('#tog').click(function(e) {
+    e.preventDefault();
+    if (auto) {
+      $('#tog').html('auto');
+      $('#gen').html('generate');
+      auto = false;
+    } else {
+      $('#tog').html('manual');
+      $('#gen').html('speed: '+ speed);
+      auto = true;
+      changeColor();
+    }
   });
   
   clip.on('success', function(e) {
@@ -26,4 +55,11 @@ function changeColor() {
   hex = '#' + tinycolor('hsl(' + col + ', 100%, 90%)').toHex(); //translate to hex
   $('#text').html(hex); //set text
   $('#text').removeClass('copied'); //clear ' - copied'
+  
+  //auto-generate colors
+  setTimeout(function() {
+    if (auto) {
+      changeColor();
+    }
+  }, speed*1000);
 }
